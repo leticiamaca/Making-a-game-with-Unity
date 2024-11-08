1b14c4af80182f95549197534b274322c80047fe
LETICIA DE CASTRO JACOB MARQUES <48194440858@info.local>
2024-11-08 17:17:11 -0300

    Feat: Exercise three

---
+++ My project/Assets/script/ExerciciosLacosDeRepeticao/ExercicioTres.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class ExercicioTres : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            if (i == 1)
+            {
+                print("Moedas coletas: " + i * 3);
+            };
+            if (i == 2)
+            {
+                print("Moedas coletas: " + i * 3);
+            };
+            if (i == 3)
+            {
+                print("Moedas coletas: " + i * 3);
+            };
+            if (i == 4)
+            {
+                print("Moedas coletas: " + i * 3);
+            };
+            if (i == 5)
+            {
+                print("Moedas coletas: " + i * 3);
+            };
+            if (i == 6)
+            {
+                print("Moedas coletas: " + i * 3);
+            };
+            if (i == 7)
+            {
+                print("Moedas coletas: " + i * 3);
+            };
+            if (i == 8)
+            {
+                print("Moedas coletas: " + i * 3);
+            };
+            if (i == 9)
+            {
+                print("Moedas coletas: " + i * 3);
+            };
+            if (i == 10)
+            {
+                print("Moedas coletas: " + i * 3);
+                print("Voce tem um total de " + i * 3 + " moedas..");
+            };
+        }
+    }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    
+}
